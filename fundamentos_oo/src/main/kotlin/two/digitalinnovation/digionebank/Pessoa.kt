3c98d2bde9218847005079b8ddd6f31f0f1aa11e
LuisCQueiroz <djlc2@yahoo.com.br>
2021-07-23 03:18:44 -0300

    Atualizando

---
+++ fundamentos_oo/src/main/kotlin/two/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,12 @@
+package two.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome:String = "Luis"
+    var cpf:String = "123.123.123-92"
+}
+fun main() {
+    val  luis = Pessoa()
+    println(luis.nome)
+    println(luis.cpf)
+
+}
