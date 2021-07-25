651be1e13ab54478f72bff447385d6f08850c6bd
LuisCQueiroz <djlc2@yahoo.com.br>
2021-07-25 19:10:10 -0300

    Atualização onde foi aplicado "Private Set" para deixar campo privado para alterações.

--- fundamentos_oo/src/main/kotlin/two/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/two/digitalinnovation/digionebank/Pessoa.kt
@@ -2,11 +2,22 @@ package two.digitalinnovation.digionebank
 
 class Pessoa {
     var nome:String = "Luis"
+
     var cpf:String = "123.123.123-92"
+    private set  //deixa privado não podendo ser alterado
+
+    /*inner class Endereco{
+        var rua:String = "Rua teste, 1234"
+    }*/
 }
 fun main() {
     val  luis = Pessoa()
+
+    //luis.cpf ="453" => como está privado
     println(luis.nome)
     println(luis.cpf)
 
+    //println(luis.Endereco().rua)
+
 }
+
